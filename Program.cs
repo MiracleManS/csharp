ffbaa279c7104907faedc3db8bb023b640cd0a5b
Dustin Sier <dustin.sier@gmail.com>
2020-09-23 21:13:43 -0400

    Added first commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace Csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			int numberguess = -1;
+			int number = 4; //new Random().Next(1, 10);
+
+			while (!CheckGuess(number, numberguess))
+			{
+				Console.WriteLine("Please enter a guess bewteen 1-10");
+				int.TryParse(Console.ReadLine(), out numberguess);
+			}
+
+			Console.WriteLine("You're the best guesser ever!");
+		}
+
+		public static bool CheckGuess(int guess, int expected)
+		{
+			if (guess == expected)
+			{
+				return true;
+			}
+
+			return false;
+		}
+    }
+}
